ef07d63faf1bd1d562096a485d9cf357d526d0c7
Satoshi Sahara <sahara.satoshi@gmail.com>
2015-04-12 23:38:01 +0900

    add config settings lang files

---
+++ lang/en/settings.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * DokuWiki Plugin ExtLink
+ * English language for settings
+ */
+
+$lang['iframe_direct'] = 'allow direct use of iframe tag in wiki pages';
+$lang['img_direct']    = 'allow direct use of img tag in wiki pages';
+
---
+++ lang/ja/settings.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * DokuWiki Plugin ExtLink
+ * Japanese language for settings
+ */
+
+$lang['iframe_direct'] = '<iframe> tag を直接HTMLに出力する';
+$lang['img_direct']    = '<img> tag を直接HTMLに出力する';
+
